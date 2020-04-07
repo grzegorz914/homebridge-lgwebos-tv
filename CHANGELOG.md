81f85aa8769a5ec6d664f1b7c0e1f83236b4608a
Grzegorz <grzegorz914@icloud.com>
2020-04-07 07:31:41 +0200

    bump 0.2.97

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,8 @@
 # Change Log
 All notable changes to this project will be documented in this file.
+## 0.2.97 (07.04.2020)
+- fixed store of positin in HomeKit fav.
+
 ## 0.2.96 (06.04.2020)
 - name corrections in TV information files
 
--- index.js
+++ index.js
@@ -168,10 +168,6 @@ class lgwebosTvDevice {
 
 		//Delay to wait for device info before publish
 		setTimeout(this.prepareTvService.bind(this), 1000);
-
-		this.tvAccesory = new Accessory(this.name, UUIDGen.generate(this.host + this.name));
-		this.log.debug('Device: %s, publishExternalAccessories: %s', this.host, this.name);
-		this.api.publishExternalAccessories('homebridge-lgwebos-tv', [this.tvAccesory]);
 	}
 
 	connect() {
@@ -350,6 +346,8 @@ class lgwebosTvDevice {
 	//Prepare TV service 
 	prepareTvService() {
 		this.log.debug('prepereTvService');
+		this.tvAccesory = new Accessory(this.name, UUIDGen.generate(this.host + this.name));
+		
 		this.tvService = new Service.Television(this.name, 'tvService');
 		this.tvService.setCharacteristic(Characteristic.ConfiguredName, this.name);
 		this.tvService.setCharacteristic(Characteristic.SleepDiscoveryMode, Characteristic.SleepDiscoveryMode.ALWAYS_DISCOVERABLE);
@@ -381,6 +379,9 @@ class lgwebosTvDevice {
 		this.tvAccesory.addService(this.tvService);
 		this.prepereTvSpeakerService();
 		this.prepareInputServices();
+
+		this.log.debug('Device: %s, publishExternalAccessories: %s', this.host, this.name);
+		this.api.publishExternalAccessories('homebridge-lgwebos-tv', [this.tvAccesory]);
 	}
 
 	//Prepare speaker service
